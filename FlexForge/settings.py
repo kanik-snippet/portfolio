3a4bc9c3af8ce765e6ab4a0bef456cc37090ace4
kanik-snippet <bunnygupta578@gmail.com>
2025-09-28 01:03:18 +0530

    Update Settings.py

--- FlexForge/settings.py
+++ FlexForge/settings.py
@@ -9,7 +9,6 @@ Created at: March 16, 2025
 
 from pathlib import Path
 from decouple import config, Csv
-from csp.constants import SELF, NONE, UNSAFE_INLINE
 import os
 # ------------------------------------------------------------------------------
 # BASE SETTINGS
@@ -62,72 +61,10 @@ SECURE_REFERRER_POLICY = 'strict-origin-when-cross-origin'
 X_FRAME_OPTIONS = 'DENY'
 
 # Content Security Policy settings
-CONTENT_SECURITY_POLICY = {
-    'DEFAULT_SRC': [SELF],
-    'DIRECTIVES': {
-        'base-uri': [SELF],
-        'connect-src': [
-            SELF,
-            'ridwaanhall.com',
-            '*.googleapis.com',
-            'https://api.web3forms.com',
-            'api.web3forms.com',
-        ],
-        'default-src': [SELF],
-        'font-src': [
-            SELF,
-            'ridwaanhall.com',
-            '*.gstatic.com',
-        ],
-        'form-action': [
-            SELF,
-            'api.web3forms.com',
-        ],
-        'frame-ancestors': [NONE],
-        'frame-src': [
-            SELF,
-            '*.google.com',
-        ],
-        'img-src': [
-            SELF,
-            'ridwaanhall.com',
-            'data:',
-            BLOG_BASE_IMG_URL,
-            PROJECT_BASE_IMG_URL,
-            'cdn.jsdelivr.net',
-            'wsrv.nl',
-            '*.googleapis.com',
-            '*.gstatic.com',
-            'lh3.googleusercontent.com',
-            'avatars.githubusercontent.com',
-        ],
-        'object-src': [NONE],
-        'script-src': [
-            SELF,
-            UNSAFE_INLINE,
-            'ridwaanhall.com',
-            'static.cloudflareinsights.com',
-            '*.googleapis.com',
-            'cdn.jsdelivr.net',
-        ],
-        'style-src': [
-            SELF,
-            UNSAFE_INLINE,
-            'ridwaanhall.com',
-            '*.googleapis.com',
-            '*.gstatic.com',
-            'cdn.jsdelivr.net',
-        ],
-        'upgrade-insecure-requests': False,  # Enable in production only
-    },
-    # 'REPORT_ONLY': DEBUG,  # Test in development mode
-    # 'REPORT_URI': '/csp-report/' if True else None,  # Report violations in production
-}
+
 
 # Filter directives with None values
-for directive, values in CONTENT_SECURITY_POLICY['DIRECTIVES'].items():
-    if isinstance(values, list):
-        CONTENT_SECURITY_POLICY['DIRECTIVES'][directive] = [v for v in values if v is not None]
+
 
 # Permissions Policy
 PERMISSIONS_POLICY = {
@@ -177,8 +114,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
     
-    # Security apps
-    "csp",
+
     
     # Project apps
     'apps.core',
@@ -208,7 +144,6 @@ if GUESTBOOK_PAGE:
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    'csp.middleware.CSPMiddleware',
     'django_permissions_policy.PermissionsPolicyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
